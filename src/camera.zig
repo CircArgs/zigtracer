15c72a981641e320854c42f719eda414d9d635f4
circargs <nick@ouellet.dev>
2023-08-16 20:17:26 -0400

    camera refactor

--- src/camera.zig
+++ src/camera.zig
@@ -21,15 +21,16 @@ pub fn init(aspect_ratio: f32, image_width: u32) Self {
 
 pub fn render(self: *const Self, world: *const objects.HittableList, stream: *const std.fs.File) !void {
     const settings = CameraSettings.init(self.aspect_ratio, self.image_width);
+    try stream.writer().print("P3\n{} {}\n255\n", .{ settings.image_width, settings.image_height });
     var progress: f32 = 0;
-    const total_pixels = settings.image_width * settings.image_height;
+    const total_pixels = @as(f32, @floatFromInt(settings.image_width * settings.image_height));
     for (0..settings.image_height) |j| {
         try utils.printProgressBar(@intFromFloat(progress / total_pixels));
         for (0..settings.image_width) |i| {
             const pixel_loc = settings.pixel_origin.add(&settings.pixel_delta_v.scalarMultiply(@as(f32, @floatFromInt(j))).add(&settings.pixel_delta_u.scalarMultiply(@as(f32, @floatFromInt(i)))));
-            const ray_direction = pixel_loc.subtract(&settings.camera_center);
-            const pixel_color = rayColor(&Ray.init(&settings.camera_center, ray_direction), &world);
-            try color.writeColor(&stream, &pixel_color);
+            const ray_direction = pixel_loc.subtract(&settings.center);
+            const pixel_color = rayColor(&Ray.init(settings.center, ray_direction), world);
+            try color.writeColor(stream, &pixel_color);
 
             progress += 100;
         }
@@ -58,22 +59,24 @@ const CameraSettings = struct {
     pixel_delta_v: Vec3,
 
     pub fn init(aspect_ratio: f32, image_width: u32) CameraSettings {
-        var image_height = @as(i32, @floor(image_width / aspect_ratio));
-        if (image_height < 1) {
-            image_height = 1;
+        const image_width_ = @as(f32, @floatFromInt(image_width));
+        var image_height_ = @floor(image_width_ / aspect_ratio);
+        if (image_height_ < 1) {
+            image_height_ = 1;
         }
+        const image_height = @as(u32, @intFromFloat(image_height_));
 
         // Determine viewport dimensions.
-        const viewport_height: comptime_float = 2.0;
-        const viewport_width: comptime_float = viewport_height * @as(f32, @floatCast(image_width)) / @as(f32, @floatCast(image_height));
+        const viewport_height: f32 = 2.0;
+        const viewport_width: f32 = viewport_height * image_width_ / image_height_;
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
         const viewport_u = Vec3.init(viewport_width, 0, 0);
         const viewport_v = Vec3.init(0, -viewport_height, 0);
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-        const pixel_delta_u = viewport_u.scalarDivide(image_width);
-        const pixel_delta_v = viewport_v.scalarDivide(image_height);
+        const pixel_delta_u = viewport_u.scalarDivide(image_width_);
+        const pixel_delta_v = viewport_v.scalarDivide(image_height_);
         const center = Point3.init(0, 0, 0);
 
         // Calculate the location of the upper left pixel.
--- src/main.zig
+++ src/main.zig
@@ -12,12 +12,9 @@ const HittableList = objects.HittableList;
 const Interval = core.Interval;
 const Camera = @import("camera.zig");
 
-const printProgressBar = @import("utils.zig").printProgressBar;
-
 const IMAGE_FILE = "image.ppm";
 const ASPECT_RATIO: comptime_float = 16.0 / 9.0;
-const IMG_HEIGHT: comptime_int = 100;
-const IMG_WIDTH: comptime_int = @floor(IMG_HEIGHT * ASPECT_RATIO);
+const IMG_WIDTH: comptime_int = 225;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -26,12 +23,11 @@ pub fn main() !void {
 
     var file = try std.fs.cwd().createFile(IMAGE_FILE, std.fs.File.CreateFlags{ .read = true });
     defer file.close();
-    try file.writer().print("P3\n{} {}\n255\n", .{ IMG_WIDTH, IMG_HEIGHT });
 
     var world = HittableList.init(allocator);
     defer world.deinit();
     try world.add(Hittable.sphere(Point3.init(0, 0, -1), 0.5));
     try world.add(Hittable.sphere(Point3.init(0, -100.5, -1), 100));
     const camera = Camera.init(ASPECT_RATIO, IMG_WIDTH);
-    camera.render(&world, &file);
+    try camera.render(&world, &file);
 }
